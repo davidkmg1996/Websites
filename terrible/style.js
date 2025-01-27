0369102bd53bf1ae627bb693b99f7c9417c03e0e
David Markowski-Goldwyn <markdk01@pfw.edu>
2025-01-27 15:48:11 -0500

    removed copied comments

--- terrible/style.js
+++ terrible/style.js
@@ -4,8 +4,9 @@ function openNav() {
     document.getElementById("main").style.marginLeft = "250px";
   }
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+  //Terrible
