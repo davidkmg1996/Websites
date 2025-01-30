3f6a5a283d40f5faeaac53e88c05fffb167fdef2
David Markowski-Goldwyn <markdk01@pfw.edu>
2025-01-30 06:08:11 -0500

    login/signup

---
+++ pizzaplanet/pizzaplanet/templates/style.js
@@ -0,0 +1,66 @@
+const pizzaSpain = document.getElementById("pizza");
+
+/*
+
+getElementById() for DOM ids
+querySelector() for DOM classes
+
+or, if you want to be particularly annoying,
+
+getElementsByClassName()
+
+*/
+
+const tScale = document.getElementById(".tOut")
+
+
+let rotation = 0;
+let scale = 1;
+let flag = 0;
+let nScale = 1;
+
+function  startRotation() {
+let rFlag = 0;
+setInterval(() => {
+  rotation += 360; 
+  scale = scale === 1 ? 0.5 : 0.8;
+  pizzaSpain.style.transform = `rotate(${rotation}deg)  scale(${scale})`; 
+}, 500);}
+
+
+// function startScale(anchor) {
+//   scale = scale === 1 ? 1 : 4;
+//   anchor.style.transform = `scale(${scale})`;
+// }
+
+function startScale(anchor) {
+  nScale = nScale === 1 ? 1.5 : 1;
+  anchor.style.transform = `scale(${nScale})`; 
+  anchor.style.transition = "transform 0.2s ease"; 
+}
+
+
+function openNav() {
+  if (flag == 0) {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    flag = 1;
+  } else {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    flag = 0
+  }
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    flag = 0;
+  }
+
+  function closeNavB() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  //Terrible
